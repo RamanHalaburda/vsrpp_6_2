ca0ee1e2195d3bf2c1c05731cca5e19978e8bcf3
RomanHalaburda <mtbrider94@gmail.com>
2017-04-07 02:32:02 +0300

    Created classes Book.cs and ListBook.cs.

--- vsrpp_6_2/Book.cs
+++ vsrpp_6_2/Book.cs
@@ -1,23 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace vsrpp_6_2
 {
-    public partial class Form1 : Form
+    class Book
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-
-        /*
+         /*
             2. Написать делегат, с помощью которого реализуется сортировка
             книг.
             Книга представляет собой класс с полями Название, Автор,
@@ -28,32 +19,23 @@ namespace vsrpp_6_2
             в качестве параметра. Каждый экземпляр делегата должен сравнивать книги
             по какому-то одному полю: названию, автору, издательству.
         */
-        
-        //delegate void myDel(int x);
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            Book b = new Book();
-            StartSort sort;
-            BookList bl = new BookList();
-            sort = new StartSort(bl.Sort);            
-            
-            sort(1);
-            // must print;
 
-            sort(2);
-            // must print;
+        public String title {get; set; }
+        public String author { get; set; }
+        public String publishingHouse { get; set; }
 
-            sort(3);
-            // must print;
-
-
-            //myDel md = new myDel(start);
-            //md(10);
+        public Book()
+        { 
+            this.title = "";
+            this.author = "";
+            this.publishingHouse = "";
         }
-        /*
-        static void start(int x)
-        { x = 10; }
-        static void stop(int x)
-        { x = 0; }*/
+
+        public Book(String _t, String _a, String _ph)
+        {
+            this.title = _t;
+            this.author = _a;
+            this.publishingHouse = _ph;
+        }        
     }
 }
---
+++ vsrpp_6_2/BookList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vsrpp_6_2
+{
+    /*
+        2. Написать делегат, с помощью которого реализуется сортировка
+        книг.
+        Книга представляет собой класс с полями Название, Автор,
+        Издательство и конструктором.
+        Создать класс, являющийся контейнером для множества книг (массив
+        книг). В этом классе предусмотреть метод сортировки книг. Критерий
+        сортировки определяется экземпляром делегата, который передается методу
+        в качестве параметра. Каждый экземпляр делегата должен сравнивать книги
+        по какому-то одному полю: названию, автору, издательству.
+    */
+    public delegate void StartSort(UInt16 _op);
+    class BookList
+    {
+        private List<Book> listBook;
+
+        public BookList() { listBook = new List<Book>(); }
+
+        public void Sort(UInt16 _op)        {
+            List<Book> sortedBook = new List<Book>();            switch(_op)            {                case 1:                    // sort by title
+                    
+                    //listBook.Sort((x, y) => x.title.CompareTo(y.title));
+                    
+                    /*
+                    listBook.Sort( (x, y) =>
+                        x == null ? (y == null ? 0 : -1)
+                        : (y == null ? 1 : x.title.CompareTo(y.title))                        );                    */
+                    
+                    //var newList = listBook.Sort(n => n.title);
+                                        //List<Book> sortedList = listBook.OrderBy(o => o.title).ToList();                    var temp1 = from o in listBook orderby o.title select o;
+                    sortedBook = temp1.ToList();                    break;                case 2:                    // sort by author                    var temp2 = from o in listBook orderby o.title select o;
+                    sortedBook = temp2.ToList();                    break;                case 3:                    // sort by publishingHouse                    var temp3 = from o in listBook orderby o.title select o;
+                    sortedBook = temp3.ToList();                    break;                        }        }
+
+    }
+}
